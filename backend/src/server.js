a9d88d8abd7d67b6598985425beeab07e6716bb5
- <debnathaniket41@gmail.com>
2026-01-02 21:15:50 +0530

    server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,8 @@ import chatRoutes from "./routes/chat.js";
 import { connectDB } from "./lib/database.js";
 
 /* ==================== FIX __dirname ==================== */
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /* ==================== APP SETUP ==================== */
 const app = express();
@@ -37,15 +37,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 /* ==================== PRODUCTION FRONTEND ==================== */
-// if (process.env.NODE_ENV === "production") {
-//   const frontendPath = path.join(__dirname, "../frontend/dist");
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-//   app.use(express.static(frontendPath));
+  app.use(express.static(frontendPath));
 
-//   app.get("*", (req, res) => {
-//        res.sendFile(path.join(frontendPath, "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+       res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
 /* ==================== START SERVER ==================== */
 app.listen(port, async () => {
