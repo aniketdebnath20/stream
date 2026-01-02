3fd626e9a8b28413c37ac66d673e8c5a002075ba
- <debnathaniket41@gmail.com>
2026-01-02 11:55:54 +0530

    url of backend change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = "https://stream-xi.vercel.app/api"
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
 });
+
+// stream-xi.vercel.app
